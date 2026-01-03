1e52651627d2e77457a3f914eb48bc64c58d80d4
Rafael <r.dantasfernandes@gmail.com>
2026-01-02 23:57:00 -0300

    Adiciona registro de modelos no admin

--- contratus/admin.py
+++ contratus/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import User, Cliente, Construtora, Equipe, Empreendimento, UnidadeEmpreendimento, Proposta, Contrato
+
+# Registra os modelos
+admin.site.register(User)
+admin.site.register(Cliente)
+admin.site.register(Construtora)
+admin.site.register(Equipe)
+admin.site.register(Empreendimento)
+admin.site.register(UnidadeEmpreendimento)
+admin.site.register(Proposta)
+admin.site.register(Contrato)
